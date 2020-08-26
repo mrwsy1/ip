d1f7cb0576d660ea03575970cf535b47eff88116
mrwsy1 <e0406495@u.nus.edu>
2020-08-26 23:53:30 +0800

    Add level2 list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,19 +11,35 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?\n");
 
+        String[] array = new String[100];
         int looper = 1;
+        int listNum = 0;
         while(looper==1) {
             String line;
             String byeStr = "bye";
+            String listStr = "list";
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             if (line.equalsIgnoreCase(byeStr)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 looper=0;
-            } else {
-                System.out.println(line);
+            } else if (line.equalsIgnoreCase(listStr)){
+                displayList(array);
             }
-        }
+            else {
+                array[listNum] = line;
+                System.out.println("added: " + line);
+                listNum++;
+            }
+        } //end while loop
+    } //end of main
 
-    }
+    public static void displayList(String[] array){
+        for (int i=0; i<array.length; i++){
+            String item = array[i];
+            if (item==null)
+                break;
+            System.out.println(i+1 + ". " + item);
+        }
+    } //end of displayList
 }
