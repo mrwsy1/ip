ae950093380c0a301858a80a8d22cfb1adae6759
mrwsy1 <e0406495@u.nus.edu>
2020-08-27 15:59:11 +0800

    Add level3 mark feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,35 +11,51 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?\n");
 
-        String[] array = new String[100];
-        int looper = 1;
-        int listNum = 0;
-        while(looper==1) {
-            String line;
-            String byeStr = "bye";
-            String listStr = "list";
-            Scanner in = new Scanner(System.in);
+        Task[] array = new Task[100];
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
             line = in.nextLine();
-            if (line.equalsIgnoreCase(byeStr)) {
+            if (line.equalsIgnoreCase("bye")) { //exit command
                 System.out.println("Bye. Hope to see you again soon!");
-                looper=0;
-            } else if (line.equalsIgnoreCase(listStr)){
-                displayList(array);
-            }
-            else {
-                array[listNum] = line;
+                break;
+            } else if (line.equalsIgnoreCase("list")){ //list command
+                if (Task.getNumTask()==0){
+                    System.out.println("No tasks recorded.");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    displayList(array);
+                }
+            } else if (line.trim().toLowerCase().startsWith("done ")){ //done command
+                String taskIndexStr = line.trim().substring(5);
+                int taskIndex = Integer.parseInt(taskIndexStr);
+                if (taskIndex==0 || taskIndex>Task.getNumTask())
+                    System.out.println("Invalid input.");
+                else if (array[taskIndex-1].isDone)
+                    System.out.println("Task is already done.");
+                else {
+                    array[taskIndex-1].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + array[taskIndex-1].getStatusIcon()
+                            + "] " + array[taskIndex-1].getDescription());
+                }
+            } else { //add command
+                Task newTask = new Task(line);
+                array[Task.getNumTask()-1] = newTask;
                 System.out.println("added: " + line);
-                listNum++;
             }
         } //end while loop
     } //end of main
 
-    public static void displayList(String[] array){
-        for (int i=0; i<array.length; i++){
-            String item = array[i];
-            if (item==null)
-                break;
-            System.out.println(i+1 + ". " + item);
+    public static void displayList(Task[] array){
+        for (int i=0; i<Task.getNumTask(); i++){
+//            if (array[i].getDescription()==null)
+//                break;
+            System.out.println(i+1 + ". [" + array[i].getStatusIcon()
+                    + "] " + array[i].getDescription());
         }
     } //end of displayList
 }
+//done 2
+//012345
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected static int numTask = 0; //track total no. of task
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        numTask++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public static int getNumTask(){
+        return numTask;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    /* how to use:
+    Task t = new Task("read book");
+    t.markAsDone()
+    */
+}
